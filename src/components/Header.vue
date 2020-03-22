ba4f33b263775c474104e67b88add21f8573d2e1
Daniel Brinkmann <d-brinkmann@gmx.net>
2020-03-22 12:19:50 +0100

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="images/Element 3@3x.png" v-bind:style="{width : 'auto', height : 'auto', maxWidth : maxWidth + 'px', maxHeight : maxHeight + 'px'}" alt="">
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        maxWidth:Number,
+        maxHeight:Number,
+    }
+}
+</script>
