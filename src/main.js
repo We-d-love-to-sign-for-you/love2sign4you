def7e82b78bbf8eadc79e92a5f6ff7aa8245cfb7
Mikhail Gaus <mikhail.gaus@ges.thm.de>
2020-03-22 21:48:15 +0100

    Added apollo

--- src/main.js
+++ src/main.js
@@ -5,13 +5,20 @@ import store from './store'
 import 'carbon-components/css/carbon-components.css';
 import CarbonComponentsVue from '@carbon/vue/src/index';
 import Header from "./components/Header";
+import VueApollo from "vue-apollo";
+import { client } from './vue-apollo';
 
 Vue.config.productionTip = false;
 Vue.use(CarbonComponentsVue);
 
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+});
+
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app');
 
---
+++ src/vue-apollo.js
@@ -0,0 +1,26 @@
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+
+const getHeaders = () => {
+  const headers = {};
+  const token = process.env.VUE_APP_APOLLO_API_KEY || '';
+  if (token) {
+    headers['x-api-key'] = token;
+  }
+  return headers;
+};
+// Create an http link:
+const link = new HttpLink({
+  uri: process.env.VUE_APP_GRAPHQL_HTTP,
+  fetch,
+  headers: getHeaders()
+});
+export const client = new ApolloClient({
+  link: link,
+  connectToDevTools: true,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
