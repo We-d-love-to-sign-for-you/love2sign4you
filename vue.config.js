ab65fcdb9c36bcefa8dd1a40607eccd9d79b5c7e
Mikhail Gaus <mikhail.gaus@ges.thm.de>
2020-03-29 12:37:01 +0200

    changed config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 // config.js file to ubdate the BASE_URL in the project to the project name
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/' + process.env.CI_PROJECT_NAME + '/'
-      : '/'
-  }
+    publicPath: '/'
+  };
